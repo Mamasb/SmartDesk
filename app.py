cc9baf84d1fb07e85122c7e10207d3e3d0f162ba
mamasb <pkpipimwangi@gmail.com>
2024-09-01 19:07:16 +0300

    added routes to different pages

--- app.py
+++ app.py
@@ -5,6 +5,31 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+    
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+    
+@app.route("/login")
+def login():
+    return render_template("login.html")
+    
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+    
+@app.route("/StudentPortal")
+def StudentPortal():
+    return render_template("StudentPortal.html")
+    
+@app.route("/TeachersDashboard")
+def TeachersDashboard():
+    return render_template("TeachersDashboard.html")
+    
+@app.route("/ParentsDashboard")
+def ParentsDashboard():
+    return render_template("ParentsDashboard.html")
+
 
 if __name__ in  "__main__":
     app.run(debug=True)
